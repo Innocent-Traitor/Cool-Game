89d93cb5f615a32b5d22715729efbcf981fa11f3
Innocent-Traitor <jamesmoffett101@gmail.com>
2024-02-01 18:28:39 -0600

    interaction box

--- Player.gd
+++ Player.gd
@@ -15,8 +15,26 @@ func _process(_delta: float) -> void:
 
 func handle_movement() -> void:
 	var dir = Input.get_vector("left", "right", "up", "down")
-	velocity = dir * speed
 	move_and_slide()
+	print(dir)
+
+	match dir:
+		Vector2(1, 0):
+			$InteractBody.position = Vector2(50, 0)
+		Vector2(-1, 0):
+			$InteractBody.position = Vector2(-50, 0)
+		Vector2(0, 1):
+			$InteractBody.position = Vector2(0, 50)
+		Vector2(0, -1):
+			$InteractBody.position = Vector2(0, -50)
+
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed('next') and not is_busy:
+		var body = $InteractBody.get_overlapping_areas()
+		do_interact(body[0])
+		print('interacting with ' + str(body[0]))
 
 
 func do_interact(obj : Node) -> void:
